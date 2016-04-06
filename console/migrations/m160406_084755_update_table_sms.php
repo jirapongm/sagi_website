a55c2386d86f9b10284dfaf71aa4501fe6600e90
Maneerin Tocharoenkul <Maneerin Tocharoenkul>
2016-04-06 15:57:05 +0700

    create table sms

---
+++ console/migrations/m160406_084755_update_table_sms.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160406_084755_update_table_sms extends Migration
+{
+    public function up()
+    {
+		$this->renameTable('table_sms', 'sms');
+    }
+
+    public function down()
+    {
+        echo "m160406_084755_update_table_sms cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
